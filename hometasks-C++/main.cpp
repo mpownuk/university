c2f677b5ea776924cbbd145c4313667d50ded2f4
icm213 <marekpownuk@gmail.com>
2023-11-11 09:34:13 +0100

    guess number game written

--- hometasks-C++/main.cpp
+++ hometasks-C++/main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <cmath>
+#include <cstdlib>
 
 using namespace std;
 
 int main()
 {
+   /*
     double x, n, result;
 
     char c;
@@ -23,6 +25,35 @@ int main()
             cout << "Pierwiastek stopnia " << n << " z liczby " << x << " to: " << result << endl;
         }
     } while (c != 'q' && c != 'Q');
+    */
+
+    srand((unsigned) time(NULL));
+
+        int randomNr = 1 + rand() % 100;
+        int x, counter = 10;
+        cout << "== gra polegajaca na odgadnieciu losowej liczby z zakresu od 1 do 100 ==" << endl;
+        cout << "== posiadasz 10 szans by odgadanc liczbe ==" << endl;
+
+        while ( counter >= 1){
+        cout << "wprowadz liczbe od 1 do 100:" << endl;
+            cin >> x;
+            if (x < 1 || x > 100){
+                cout << "podana liczba jest nieprawidłowa" << endl;
+            } else if(x == randomNr){
+                cout << "gratulacje! udalo ci sie odgadnac liczbe" << endl;
+                break;
+            } else if (x < randomNr){
+                cout << "zgadywana liczba jest Wieksza od podanej przez ciebie" << endl;
+                counter--;
+            } else {
+                cout << "zgadywana liczba jest Mniejsza od podanej przez ciebie" << endl;
+                counter--;
+            }
+            cout << "pozostalo ci " << counter << " szans." << endl;
+
+        }
+        cout << "szukana liczba to: " << randomNr << " gra zakonczona" << endl;
+
 
     return 0;
 }
--- hometasks-C/hometask-c/main.c
+++ hometasks-C/hometask-c/main.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main()
 {
+
+/*
     double x, n, result;
     char c;
 
@@ -21,6 +25,37 @@ int main()
             printf("Pierwiastek stopnia %.2lf z liczby %.2lf to: %.2lf\n", n, x, result);
         }
     } while (c != 'q' && c != 'Q');
+    */
+
+     srand((unsigned)time(NULL));
+
+    int randomNr = 1 + rand() % 100;
+    int x, counter = 10;
+    printf("== gra polegajaca na odgadnieciu losowej liczby z zakresu od 1 do 100 ==\n");
+    printf("== posiadasz 10 szans by odgadanc liczbe ==\n");
+
+    while (counter >= 1) {
+        printf("wprowadz liczbe od 1 do 100:\n");
+        scanf("%d", &x);
+
+        if (x < 1 || x > 100) {
+            printf("podana liczba jest nieprawidlowa\n");
+        } else if (x == randomNr) {
+            printf("gratulacje! udalo ci sie odgadnac liczbe\n");
+            break;
+        } else if (x < randomNr) {
+            printf("zgadywana liczba jest Wieksza od podanej przez ciebie\n");
+            counter--;
+        } else {
+            printf("zgadywana liczba jest Mniejsza od podanej przez ciebie\n");
+            counter--;
+        }
+
+        printf("pozostalo ci %d szans.\n", counter);
+    }
+
+    printf("szukana liczba to: %d gra zakonczona\n", randomNr);
+
 
     return 0;
 }
