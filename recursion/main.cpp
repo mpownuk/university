c689b14f633530a342e89e54e1faa2576d38c064
icm213 <marekpownuk@gmail.com>
2024-02-01 19:30:01 +0100

    playing with recursion

--- recursion/main.cpp
+++ recursion/main.cpp
@@ -53,13 +53,97 @@ long int userSum(long int m, long int n){
     }
 }
 
+int addTwo(int n){
+    int called = 0;
+    if(n <= -2){
+    called = 3;
+        return called;
+    } else {
+        called = addTwo(n-1)+2;
+        cout << called << endl;
+        return called;
+    }
+}
+
+long int potega( int x, int n){
+    if (n<=0) {
+        return 1;
+    } else {
+        return x * potega(x, n-1);
+    }
+}
+
+int fib(int n){
+    if(n <=1 || n==2) {
+        return 1;
+    }
+    else {
+        return fib(n-1) + fib(n-2);
+    }
+}
+
+long int silnia ( int n){
+    if (n<=0){
+        return 1;
+    } else{
+        return n * silnia(n-1);
+    }
+}
+
+int SDW(int n){
+    if ( n==1) {
+        return 1;
+    }
+    else{
+        if( (n%2) ==0){
+            return SDW(n-2)*n;
+        } else {
+            return SDW(n-1)*n;
+        }
+    }
+}
 int main()
 {
+
+
+cout << SDW(3) << endl;
+/*
+cout << silnia(1)<< endl;
+cout << silnia(2)<< endl;
+cout << silnia(3)<< endl;
+cout << silnia(4)<< endl;
+cout << silnia(5)<< endl;
+
+cout << fib(1) << endl;
+cout << fib(2) << endl;
+cout << fib(3) << endl;
+cout << fib(4) << endl;
+cout << fib(5) << endl;
+cout << fib(46) << endl;
+
+cout << potega(2,0) << endl;
+cout << potega(2,1) << endl;
+cout << potega(2,2) << endl;
+cout << potega(2,3) << endl;
+cout << potega(2,4) << endl;
+
 int a,b;
 cin >> a;
 cin >>b;
 
 cout << userSum(a, b);
+cout << endl;addTwo(-2);
+cout << endl;addTwo(-1);
+cout << endl;addTwo(0);
+cout << endl;addTwo(1);
+cout << endl;addTwo(2);
+cout << endl;addTwo(3);
+cout << endl;addTwo(4);
+cout << endl;addTwo(5);
+cout << endl;addTwo(6);
+cout << endl;addTwo(7);
+cout << endl;
+*/
 /*
     for ( int i = 0; i <= 12; i++){
         cout << c(i) << endl;
