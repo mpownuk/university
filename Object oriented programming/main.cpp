6191ddbda2a6b2da5db45f500c00f9104af86f75
icm213 <marekpownuk@gmail.com>
2024-02-13 18:30:49 +0100

    oop practicing

--- Object oriented programming/main.cpp
+++ Object oriented programming/main.cpp
@@ -2,9 +2,14 @@
 
 using namespace std;
 
-class Employee {
-private:
+class AbstractEmployee{
+    virtual void askForPromotion() = 0;
+};
+
+class Employee : AbstractEmployee {
+protected:
     string Name;
+private:
     string Company;
     int Age;
 public:
@@ -12,6 +17,16 @@ public:
         Name = name;
         Company = company;
         Age = age;
+    }
+    void askForPromotion(){
+        if(Age > 30){
+            cout << Name << " got promoted!!" << endl;
+        } else {
+            cout << Name << " didn't got promoted!!" << endl;
+        }
+    }
+    virtual void work(){
+        cout << Name << " is working on papers." << endl;
     }
 
     void setName(string name){
@@ -27,7 +42,8 @@ public:
         return Company;
     }
     void setAge(int age){
-        Age = age;
+        if(age >=18)
+            Age = age;
     }
     int getAge(){
         return Age;
@@ -40,6 +56,34 @@ public:
     };
 };
 
+class Developer : public Employee {
+public:
+    string FavProgrammingLanguage;
+    Developer(string name, string company, int age, string favProgrammingLanguage): Employee  (name, company, age){
+        FavProgrammingLanguage = favProgrammingLanguage;
+    }
+    void fixBugs(){
+        cout << Name << " fixed bugs!" << endl;
+    }
+//    void work(){
+ //       cout << Name << " is writing code." << endl;
+  //  }
+};
+
+class Teacher: public Employee {
+public:
+    string Subject;
+    Teacher(string name, string company, int age, string subject): Employee  (name, company, age){
+        Subject = subject;
+    }
+    void prepareLesson(){
+        cout << Name << " is preparing " << Subject << " lesson." << endl;
+    };
+        void work(){
+        cout << Name << " is guiding lesson." << endl;
+    }
+};
+
 int main()
 {
         Employee Adam = Employee("Adam", "IBM", 21);
@@ -47,5 +91,27 @@ int main()
 
         Employee Basia  = Employee("Basia", "XYZ", 33);
         Basia.IntrodueYourself();
+
+        Basia.setAge(12);
+        cout << Basia.getAge() << endl;
+        Basia.askForPromotion();
+        Adam.askForPromotion();
+
+        Developer Celina = Developer("Celina", "ABC", 29, "cobol");
+        Celina.fixBugs();
+        Celina.askForPromotion();
+
+        Teacher Byk = Teacher("Byk", "Ist school of being salty", 44, "math");
+        Byk.prepareLesson();
+
+        Adam.work();
+        Celina.work();
+        Byk.work();
+
+        Employee * e1 = & Celina;
+        Employee * e2 = & Byk;
+
+        e1->work();
+        e2->work();
     return 0;
 }
